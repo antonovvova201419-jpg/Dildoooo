9660d3b1b44a6cb185700c5e9e8868c413fc5896
antonovvova201419-jpg <antonovvova201419@gmail.com>
2026-03-26 18:09:49 +0200

    Update API key in _db.js

--- api/_db.js
+++ api/_db.js
@@ -1,6 +1,6 @@
 // api/_db.js
 const BIN_ID = '69c45b3fb7ec241ddca2dc2b'; // Вставь свой Bin ID
-const API_KEY = '$2a$10$wX87kHwyff/dL8hvw8Wk8.nJMpziB6dSmN/t00ZIhj6Q/m4u2ya46'; // Вставь свой Master Key
+const API_KEY = '$2a$10$XTLLifSqHT/TcrsgPqSid.ncpMjtRPW7nh2uVsXTPNY3TW7Wu5fKa'; // Вставь свой Master Key
 
 export async function getData() {
   const res = await fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}/latest`, {
